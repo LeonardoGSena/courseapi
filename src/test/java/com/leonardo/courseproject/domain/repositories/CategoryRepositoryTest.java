24a83fa0284ffb94e891f5d9bf821def8c0ba172
Leonardo Sena <lgomesgabriel@hotmail.com>
2022-06-11 14:16:34 -0300

    Delete should throw Exception when id doesn't exist.

--- src/test/java/com/leonardo/courseproject/domain/repositories/CategoryRepositoryTest.java
+++ src/test/java/com/leonardo/courseproject/domain/repositories/CategoryRepositoryTest.java
@@ -2,11 +2,15 @@ package com.leonardo.courseproject.domain.repositories;
 
 import com.leonardo.courseproject.domain.models.Category;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 public class CategoryRepositoryTest {
@@ -14,14 +18,27 @@ public class CategoryRepositoryTest {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    private Long existId;
+    private Long nonExistId;
+
+    @BeforeEach
+    void Setup() throws Exception {
+        existId = 1L;
+        nonExistId = 2L;
+    }
 
     @Test
     public void deleteShouldDeleteObjectWhenIdExists() {
-        long existId = 1L;
         categoryRepository.deleteById(existId);
         Optional<Category> result = categoryRepository.findById(existId);
 
-        Assertions.assertFalse(result.isPresent(), "It should return false");
+        assertFalse(result.isPresent(), "It should return false");
     }
 
+    @Test
+    public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
+        assertThrows(EmptyResultDataAccessException.class, () -> {
+           categoryRepository.deleteById(nonExistId);
+        });
+    }
 }
