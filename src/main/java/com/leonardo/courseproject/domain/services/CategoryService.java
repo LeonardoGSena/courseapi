1ccc8ec5e5a44bcdf393aee5d8bf6ca2625ed596
Leonardo Sena <lgomesgabriel@hotmail.com>
2022-06-24 12:08:20 -0300

    DeleteService exceptions test created and passing.Exceptions added inside delete method in CategoryService.

--- src/main/java/com/leonardo/courseproject/domain/services/CategoryService.java
+++ src/main/java/com/leonardo/courseproject/domain/services/CategoryService.java
@@ -1,7 +1,11 @@
 package com.leonardo.courseproject.domain.services;
 
 import com.leonardo.courseproject.domain.repositories.CategoryRepository;
+import com.leonardo.courseproject.domain.services.exceptions.DataBaseException;
+import com.leonardo.courseproject.domain.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 public class CategoryService {
 
@@ -9,6 +13,13 @@ public class CategoryService {
     CategoryRepository categoryRepository;
 
     public void deleteCategory(Long id) {
-        categoryRepository.deleteById(id);
+        try {
+            categoryRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Id not Found: " + id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataBaseException("Integrity violation");
+        }
+
     }
 }
--- src/test/java/com/leonardo/courseproject/domain/services/CategoryServiceTest.java
+++ src/test/java/com/leonardo/courseproject/domain/services/CategoryServiceTest.java
@@ -1,13 +1,14 @@
 package com.leonardo.courseproject.domain.services;
 
 import com.leonardo.courseproject.domain.repositories.CategoryRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,12 +23,15 @@ public class CategoryServiceTest {
     private CategoryRepository repository;
 
     private Long existingId;
+    private Long nonExistingId;
 
     @BeforeEach
     void setUp() throws Exception {
         existingId = 1L;
+        nonExistingId = 2L;
 
         Mockito.doNothing().when(repository).deleteById(existingId);
+        Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
     }
 
     @Test
@@ -35,7 +39,14 @@ public class CategoryServiceTest {
         assertDoesNotThrow(() -> {
             service.deleteCategory(existingId);
         });
-
         Mockito.verify(repository, Mockito.times(1)).deleteById(existingId);
+    }
+
+    @Test
+    public void deleteShouldThrowDataAccessExceptionWhenIdDoesNotExists() {
+        assertThrows(EmptyResultDataAccessException.class, () -> {
+            service.deleteCategory(nonExistingId);
+        });
+        Mockito.verify(repository, Mockito.times(1)).deleteById(nonExistingId);
     }
 }
