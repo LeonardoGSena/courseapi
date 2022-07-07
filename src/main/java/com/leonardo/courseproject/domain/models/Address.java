b6d5fa7f8a25b1257134f6f3872b6294133e120a
Leonardo Sena <lgomesgabriel@hotmail.com>
2022-07-07 20:37:26 -0300

    Hibernate relationship between Client, Address and City entities

--- src/main/java/com/leonardo/courseproject/domain/models/Address.java
+++ src/main/java/com/leonardo/courseproject/domain/models/Address.java
@@ -1,7 +1,14 @@
 package com.leonardo.courseproject.domain.models;
 
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_address")
 public class Address {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String street;
     private String number;
@@ -9,5 +16,102 @@ public class Address {
     private String province;
     private String zipCode;
 
+    @ManyToOne
+    @JoinColumn(name = "client_id")
     private Client client;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id")
+    private City city;
+
+    public Address() {
+    }
+
+    public Address(Long id, String street, String number, String complement, String province, String zipCode, Client client, City city) {
+        this.id = id;
+        this.street = street;
+        this.number = number;
+        this.complement = complement;
+        this.province = province;
+        this.zipCode = zipCode;
+        this.client = client;
+        this.city = city;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public void setComplement(String complement) {
+        this.complement = complement;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(id, address.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/main/java/com/leonardo/courseproject/domain/models/Client.java
+++ src/main/java/com/leonardo/courseproject/domain/models/Client.java
@@ -2,20 +2,100 @@ package com.leonardo.courseproject.domain.models;
 
 import com.leonardo.courseproject.domain.models.enums.ClientType;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import javax.persistence.*;
+import java.util.*;
 
+@Entity
+@Table(name = "tb_client")
 public class Client {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "client_name")
     private String name;
     private String email;
+    @Column(name = "EIN_or_IDno")
     private String EINOrIDNo;
-    private ClientType clientType;
+    @Column(name = "client_type")
+    private Integer clientType;
 
+    @OneToMany(mappedBy = "client")
     private List<Address> addresses = new ArrayList<>();
 
+    @ElementCollection
+    @CollectionTable(name = "tb_phone")
     private Set<String> phones = new HashSet<>();
+
+    public Client() {
+    }
+
+    public Client(Long id, String name, String email, String EINOrIDNo, ClientType clientType) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.EINOrIDNo = EINOrIDNo;
+        this.clientType = clientType.getCod();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEINOrIDNo() {
+        return EINOrIDNo;
+    }
+
+    public void setEINOrIDNo(String EINOrIDNo) {
+        this.EINOrIDNo = EINOrIDNo;
+    }
+
+    public ClientType getClientType() {
+        return ClientType.toEnum(clientType);
+    }
+
+    public void setClientType(ClientType clientType) {
+        this.clientType = clientType.getCod();
+    }
+
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+
+    public Set<String> getPhones() {
+        return phones;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(id, client.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
