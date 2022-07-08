1457099da43fe4a6df3c5ab216da0ed513b46e24
Leonardo Sena <lgomesgabriel@hotmail.com>
2022-07-08 19:59:40 -0300

    Address Repository created.

---
+++ src/main/java/com/leonardo/courseproject/domain/repositories/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.leonardo.courseproject.domain.repositories;
+
+import com.leonardo.courseproject.domain.models.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
